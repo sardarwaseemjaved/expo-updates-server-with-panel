706daaccd81035fd7ec93a762d7f954e2e36dd2e
Waseem Javed <sardarwaseemjaved@gmail.com>
2023-10-01 04:49:56 -0600

    Updated docker prod values

--- Docker/production/mongoinit/init.js
+++ Docker/production/mongoinit/init.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
-let db = connect('mongodb://admin:myPRODUCTIONPassword@localhost:27017/admin')
+// let db = connect('mongodb://admin:myPRODUCTIONPassword@localhost:27017/admin')
+let db = connect('mongodb+srv://waseemj096:CjPNGHvnKmy2dsu2@cluster0.wjzx5.mongodb.net/admin')
 
 db = db.getSiblingDB('expo')
 
